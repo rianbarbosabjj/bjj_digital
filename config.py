d095e8d01fffbef4ea002a3e8290e8b0c4eb9f07
rianbarbosabjj <rianbarbosa@gmail.com>
2025-11-22 20:49:22 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+# Configurações Visuais
+COR_FUNDO = "#0e2d26"
+COR_TEXTO = "#FFFFFF"
+COR_DESTAQUE = "#FFD770"
+COR_BOTAO = "#078B6C"
+COR_HOVER = "#FFD770"
+
+# Caminhos
+DB_PATH = os.path.expanduser("~/bjj_digital.db")
