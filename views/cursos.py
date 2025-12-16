580856f157f66786b006adfa0f326bd1bd71adc4
rianbarbosabjj <rianbarbosa@gmail.com>
2025-12-16 13:23:39 -0300

    Update cursos.py

--- views/cursos.py
+++ views/cursos.py
@@ -112,7 +112,7 @@ def pagina_cursos(usuario: dict):
     aplicar_estilos_cursos()
     if 'cursos_view' not in st.session_state: st.session_state['cursos_view'] = 'lista'
         
-    st.markdown(f"""<div class="curso-header"><h1 style="margin:0; text-align:center; color:{COR_DESTAQUE};">🎓 GERENCIE SEUS CURSOS</h1>
+    st.markdown(f"""<div class="curso-header"><h1 style="margin:0; text-align:center; color:{COR_DESTAQUE};">🎓 BJJ DIGITAL CURSOS</h1></p></div>""", unsafe_allow_html=True)
     
     if st.session_state.get('cursos_view') != 'lista':
         if st.button("← Voltar à Lista", key="btn_back_list"): navegar_para('lista')
