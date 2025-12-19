ff9b905e90f8bab1269ffb51822226ff8dac1ec8
rianbarbosabjj <rianbarbosa@gmail.com>
2025-12-19 09:27:16 -0300

    Create cursos.py

---
+++ views/cursos.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def pagina_cursos(usuario):
+    tipo = usuario.get("tipo")
+
+    if tipo in ["admin", "professor", "mestre"]:
+        from views.cursos_professor import pagina_cursos_professor
+        pagina_cursos_professor(usuario)
+    else:
+        from views.cursos_aluno import pagina_cursos_aluno
+        pagina_cursos_aluno(usuario)
